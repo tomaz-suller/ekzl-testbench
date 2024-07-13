633ee77ceaebda13103e26449d36161d8a159ce5
Tomaz Maia Suller <tomaz.maia@mail.polimi.it>
2024-07-13 16:05:20 +0200

    Refactor metric collection

--- src/main.py
+++ src/main.py
@@ -1,14 +1,15 @@
 import asyncio
 from dataclasses import dataclass, field
+import gc
 import json
 import logging
-from time import process_time_ns
+from time import process_time_ns, time_ns
 from datetime import datetime
 from typing import Any, Union
 
 import hydra
 from hydra.core.config_store import ConfigStore
-from omegaconf import OmegaConf
+from omegaconf import OmegaConf, MISSING
 
 from model import Model, ModelAttributes, export
 from proof import calibrate_settings, generate_proof, verify_proof
@@ -24,6 +25,7 @@ class Visibility:
 
 @dataclass
 class EzklConfig:
+    run_name: str = MISSING
     models: Union[list[str], None] = None
     polynomial: bool = True
     visibility: Visibility = field(default_factory=Visibility)
@@ -32,7 +34,7 @@ class EzklConfig:
     export: bool = False
     calibrate: bool = False
     calibration_samples: int = 20
-    generate: bool = True
+    generate: bool = False
     inference_samples: int = 1
     verify: bool = False
 
@@ -44,14 +46,18 @@ log = logging.getLogger(__name__)
 
 
 async def main(cfg: EzklConfig) -> None:
-    models = [
-        Model(attributes.model_name, attributes.input_shape, polynomial=cfg.polynomial)
+    selected_model_attributes = [
+        attributes
         for attributes in ModelAttributes
         if cfg.models is None or attributes.model_name in cfg.models
     ]
-    for model in models:
+    for attributes in selected_model_attributes:
+        model = Model(attributes.model_name, attributes.input_shape, polynomial=cfg.polynomial)
         log.info(f"Processing model {model.name}")
-        metrics: dict[str, Any] = {"timestamp": str(datetime.now())}
+        metrics: dict[str, Any] = {
+            "run": cfg.run_name,
+            "timestamp": datetime.now().isoformat(),
+        }
 
         if cfg.generate_calibration_data:
             with model.paths.calibration_data.open("w") as f:
@@ -64,29 +70,20 @@ async def main(cfg: EzklConfig) -> None:
                     random_input_data(cfg.inference_samples, *model.input_shape), f
                 )
 
-        if cfg.export:
-            start = process_time_ns()
-            export(model)
-            export_time = process_time_ns() - start
-            metrics["export_time"] = export_time
+        for task_name in ("export", "calibrate", "generate", "verify"):
+            if cfg[task_name]:
+                wall_start = time_ns()
+                process_start = process_time_ns()
+                await execute_task(task_name, model, OmegaConf.to_container(cfg.visibility))
+                metrics[f"{task_name}_wall_time"] = time_ns() - wall_start
+                metrics[f"{task_name}_process_time"] = process_time_ns() - process_start
 
-        if cfg.calibrate:
-            start = process_time_ns()
-            await calibrate_settings(
-                model.paths, OmegaConf.to_container(cfg.visibility)
-            )
-            calibration_time = process_time_ns() - start
-            metrics["calibration_time"] = calibration_time
+        if cfg.export:
+            metrics["export_size"] = model.paths.onnx.stat().st_size
         if cfg.generate:
-            start = process_time_ns()
-            await generate_proof(model.paths)
-            generation_time = process_time_ns() - start
-            metrics["generation_time"] = generation_time
-        if cfg.verify:
-            start = process_time_ns()
-            verify_proof(model.paths)
-            verification_time = process_time_ns() - start
-            metrics["verification_time"] = verification_time
+            metrics["proofer_key_size"] = model.paths.proofer_key.stat().st_size
+            metrics["verifier_key_size"] = model.paths.verifier_key.stat().st_size
+            metrics["proof_size"] = model.paths.proof.stat().st_size
 
         metrics_path = model.paths.metrics
         old_metrics = []
@@ -96,6 +93,20 @@ async def main(cfg: EzklConfig) -> None:
         old_metrics.append(metrics)
         with metrics_path.open("w") as f:
             json.dump(old_metrics, f)
+
+        del model
+        gc.collect()
+
+
+async def execute_task(name: str, model: Model, visibility: dict[str, str]):
+    if name == "export":
+        export(model)
+    if name == "calibrate":
+        await calibrate_settings(model.paths, visibility)
+    if name == "generate":
+        await generate_proof(model.paths)
+    if name == "verify":
+        verify_proof(model.paths)
 
 
 @hydra.main(version_base=None, config_name="config")
